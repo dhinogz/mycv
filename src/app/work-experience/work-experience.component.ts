4f6788ba1a015817057f1ab4eb5b8c8a5c465f20
dhinogz <davidhinojosagzz@gmail.com>
2023-03-08 19:49:13 -0600

    Add work experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -5,6 +5,36 @@ import { Component } from '@angular/core';
   templateUrl: './work-experience.component.html',
   styleUrls: ['./work-experience.component.scss']
 })
+
 export class WorkExperienceComponent {
 
+workExperience: Array<any> = [];
+
+constructor() { }
+
+ngOnInit(): void {
+  let work1 = {
+    fecha : "2023-Futuro",
+    ubicacion : "Seattle, WA",
+    puesto: "Software Engineer Intern",
+    empresa: "Microsoft",
+    logros : [
+      { descripcion: "Creacion de software" },
+      {descripcion: "Documentacion" },
+    ]
+  };
+
+  let work2 = {
+    fecha : "2022-Actualidad",
+    ubicacion : "San Pedro, Garza Garcia, NL",
+    puesto: "Backend Engineer Intern",
+    empresa: "malc",
+    logros : [
+      {descripcion: "Manejo y diseno de base de datos" }, 
+      {descripcion: "Desarrollo de APIs" },
+    ]
+  };
+this.workExperience.push(work1); 
+this.workExperience.push(work2);
+}
 }
